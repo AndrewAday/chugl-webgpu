48cf652460a7848705926bc627ac647ce1957f04
Andrew Aday <andrewladay@gmail.com>
2024-06-05 22:45:12 -0700

    imgui profile

--- src/tests/imgui_test.cpp
+++ src/tests/imgui_test.cpp
@@ -1,18 +1,70 @@
 #include "test_base.h"
 
+#include <GLFW/glfw3.h>
 #include <imgui/backends/imgui_impl_glfw.h>
 #include <imgui/backends/imgui_impl_wgpu.h>
 #include <imgui/imgui.h>
-
+#include <sokol/sokol_time.h>
 #include <webgpu/webgpu.h>
 
 static GraphicsContext* gctx = NULL;
 static GLFWwindow* window    = NULL;
 
+struct TickStats {
+    u64 min   = UINT64_MAX;
+    u64 max   = 0;
+    u64 total = 0;
+
+    void update(u64 ticks)
+    {
+        min = ticks < min ? ticks : min;
+        max = ticks > max ? ticks : max;
+        total += ticks;
+    }
+};
+
+struct ImGui_Stats {
+    u64 fc = 0;
+    TickStats new_frame;
+    TickStats command;
+    TickStats render;
+    TickStats draw;
+    TickStats poll;
+
+    void update(u64 new_frame_time, u64 command_time, u64 render_time,
+                u64 draw_time, u64 poll_time)
+    {
+        new_frame.update(new_frame_time);
+        command.update(command_time);
+        render.update(render_time);
+        draw.update(draw_time);
+        poll.update(poll_time);
+        fc++;
+    }
+
+    void print()
+    {
+        printf("Frame count: %llu\n", fc);
+        printf("New frame: min: %f, max: %f, avg: %f\n", stm_ms(new_frame.min),
+               stm_ms(new_frame.max), stm_ms(new_frame.total / fc));
+        printf("Command: min: %f, max: %f, avg: %f\n", stm_ms(command.min),
+               stm_ms(command.max), stm_ms(command.total / fc));
+        printf("Render: min: %f, max: %f, avg: %f\n", stm_ms(render.min),
+               stm_ms(render.max), stm_ms(render.total / fc));
+        printf("Draw: min: %f, max: %f, avg: %f\n", stm_ms(draw.min),
+               stm_ms(draw.max), stm_ms(draw.total / fc));
+        printf("Poll: min: %f, max: %f, avg: %f\n", stm_ms(poll.min),
+               stm_ms(poll.max), stm_ms(poll.total / fc));
+    }
+};
+
+static ImGui_Stats stats = {};
+
 static void _Test_ImGUI_onInit(GraphicsContext* ctx, GLFWwindow* w)
 {
     gctx   = ctx;
     window = w;
+    stm_setup();
 }
 
 static bool show_demo_window    = true;
@@ -22,12 +74,20 @@ static ImVec4 clear_color       = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 static void _Test_ImGUI_onRender(glm::mat4 proj, glm::mat4 view,
                                  glm::vec3 camPos)
 {
+    u64 poll_start = stm_now();
+    glfwPollEvents();
+    u64 poll_time = stm_since(poll_start);
+
     // Start the Dear ImGui frame
+    u64 frame_start = stm_now();
     ImGui_ImplWGPU_NewFrame();
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
+    u64 frame_start_time = stm_since(frame_start);
 
     ImGuiIO& io = ImGui::GetIO();
+
+    u64 imgui_command_start = stm_now();
 
     // 1. Show the big demo window (Most of the sample code is in
     // ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear
@@ -69,8 +129,12 @@ static void _Test_ImGUI_onRender(glm::mat4 proj, glm::mat4 view,
         ImGui::End();
     }
 
+    u64 imgui_command_time = stm_since(imgui_command_start);
+
     // Rendering (prepares ImDrawList)
+    u64 imgui_render_start = stm_now();
     ImGui::Render();
+    u64 imgui_render_time = stm_since(imgui_render_start);
 
     // pass encoder
     WGPUCommandEncoderDescriptor enc_desc = {};
@@ -98,7 +162,9 @@ static void _Test_ImGUI_onRender(glm::mat4 proj, glm::mat4 view,
       = wgpuCommandEncoderBeginRenderPass(encoder, &render_pass_desc);
 
     // actual draw call
+    u64 imgui_draw_start = stm_now();
     ImGui_ImplWGPU_RenderDrawData(ImGui::GetDrawData(), pass);
+    u64 imgui_draw_time = stm_since(imgui_draw_start);
 
     // multiple viewports (not working)
     // if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
@@ -126,6 +192,10 @@ static void _Test_ImGUI_onRender(glm::mat4 proj, glm::mat4 view,
     wgpuRenderPassEncoderRelease(pass);
     wgpuCommandEncoderRelease(encoder);
     wgpuCommandBufferRelease(cmd_buffer);
+
+    stats.update(frame_start_time, imgui_command_time, imgui_render_time,
+                 imgui_draw_time, poll_time);
+    if (stats.fc % 100 == 0) stats.print();
 }
 
 static void _Test_ImGUI_onExit()
